f628a10032a406925f69ae26bb411d6fc53336d5
Aleksandr Chasnyk <69671996+ami3go@users.noreply.github.com>
2025-07-25 11:39:19 +0300

    folder functions update

--- Functions/Voltage_measurements.py
+++ Functions/Voltage_measurements.py
@@ -22,10 +22,12 @@ def init_logger(channels=[101, 116]):
         log.send(item)
         # print(item) # debug only
         time.sleep(3)
+
 def trigger():
     log.send(cmd.init.str())
 
-def read_voltage(as_dict = False):
+
+def read_voltage():
     srt = log.query(cmd.fetch.req())
     meas_array = srt.split(",")
     for z in range(0, len(meas_array)):
@@ -33,11 +35,9 @@ def read_voltage(as_dict = False):
             meas_array[z] = round((float(meas_array[z])), 7)
         else:
             meas_array[z] = 0
-    if as_dict:
-        ret_dict = {}
-        for index, val in enumerate(meas_array):
-            # 'DAQ_1V
-            key = f"DAQ_{i+1}V"
-            ret_dict[key] = val
-        return ret_dict
     return meas_array
+
+
+if __name__ == '__main__':
+    log = logger_class.usb_interface()
+    # print(log.send("*IDN?"))
--- Functions/Voltage_measurements_test.py
+++ Functions/Voltage_measurements_test.py
@@ -1,6 +1,10 @@
+import time
+
 import Functions.Voltage_measurements as dmm
 
 dmm.init_logger()
-
+dmm.trigger()
 for i in range(5):
+    dmm.trigger()
+    time.sleep(1)
     print(dmm.read_voltage())
